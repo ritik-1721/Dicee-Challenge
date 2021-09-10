4b94fb2e32a51af78333f9b21fcf4fe8e2b259d6
Ritik pawar <83938643+ritik-1721@users.noreply.github.com>
2021-09-10 15:56:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;  // 1-6
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png"; // dice1.png -dice6.png
+
+var randomImageSource1 = "images/" + randomDiceImage1 ; // images/dice1.png -images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png" ;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML=". Play 1 Wins!";
+}
+
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins! .";
+}
+
+else {
+  document.querySelector("h1").innerHTML="Draw!";
+}
